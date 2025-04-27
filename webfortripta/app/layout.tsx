517008de85298dbced70f527781cbd4e277806ce
Googly029 <ui20cs24@iiitsurat.ac.in>
2025-04-27 16:05:19 +0530

    Set Up

---
+++ webfortripta/app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/globals.css'
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+    return (
+    <html>
+        <body>
+        {children}
+        </body>
+    </html>
+    )
+}
